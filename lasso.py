219ddebd455b2653cbc2d7b06eb5fdc99ce2820c
MystikHub <shutanu@pm.me>
2021-12-04 23:37:10 +0000

    Fixed issues caused by indentation errors

--- lasso.py
+++ lasso.py
@@ -16,7 +16,7 @@ if cross_validate:
 
     # Array where each element is the average mean error for each c value
     feature_set_c_errors = []
-        C_range = [1, 10, 100, 1000]
+    C_range = [1, 10, 100, 1000]
 
     for feature_set_number in range(1, N_FEATURE_SETS + 1):
 
@@ -43,28 +43,28 @@ if cross_validate:
                     # Get this city's data frame
                     data_frame = load_feature_sets.get_data_frame(country, city, feature_set_number)
 
-                shape = data_frame.shape
-                n_columns = shape[1]
+                    shape = data_frame.shape
+                    n_columns = shape[1]
 
-                # Find x and y for training
-                # x will be training feature sets
-                # y will be day's recorded temperature
-                x = data_frame.iloc[:, range(1, n_columns - 1)]
-                y = data_frame.iloc[:, 0]
+                    # Find x and y for training
+                    # x will be training feature sets
+                    # y will be day's recorded temperature
+                    x = data_frame.iloc[:, range(1, n_columns - 1)]
+                    y = data_frame.iloc[:, 0]
 
-                # Train the model on training data, then make predictions on the
-                #   test data. Use the predictions to measure the mean square
-                #   error
-                x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+                    # Train the model on training data, then make predictions on the
+                    #   test data. Use the predictions to measure the mean square
+                    #   error
+                    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
-                # Train the model
-                lasso_regression = Lasso(alpha=(1/C))
-                lasso_regression.fit(x_train, y_train)
+                    # Train the model
+                    lasso_regression = Lasso(alpha=(1/C))
+                    lasso_regression.fit(x_train, y_train)
 
-                y_pred = lasso_regression.predict(x_test)
-                error = mean_squared_error(y_test, y_pred)
+                    y_pred = lasso_regression.predict(x_test)
+                    error = mean_squared_error(y_test, y_pred)
 
-                total_error = total_error + error
+                    total_error = total_error + error
                     total_error_count += 1
                     del data_frame
             
