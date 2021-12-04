2caa585c97bf3a512ed444e72df2c9c189efa3d3
Stefan <shutanu@pm.me>
2021-12-04 23:23:37 +0000

    Replaced the data frame function with functions to load feature set files one by one

--- load_feature_sets.py
+++ load_feature_sets.py
@@ -95,49 +95,49 @@ def get_data_feature_sets():
 
     return data
 
-# Identical to the previous function, but instead of arrays of comma separated
-#   values, an array of {feature_set}/{city}/[data_frames] is returned
-def get_data_frames():
+def get_countries():
 
     # Check if the feature sets directory exists
     if not os.path.isdir(FEATURE_SET_DIRECTORY):
-        print("Couldn't find any feature sets")
+        print("Couldn't find feature sets for any country")
+        print("Did you run 'python3 make-feature-sets.py'?")
+        exit(1)
+    
+    countries = os.listdir(FEATURE_SET_DIRECTORY)
+    countries.sort()
+    return countries
+
+def get_cities(country_name):
+
+    # Check if the country's directory exists
+    if not os.path.isdir("{}/{}".format(FEATURE_SET_DIRECTORY, country_name)):
+        print("Couldn't find feature sets for the country " + country_name)
         print("Did you run 'python3 make-feature-sets.py'?")
         exit(1)
 
-    # Dictionary to store our feature sets
-    # Each element in this array will correspond to one of the three feature sets
-    # We defined in the "make_feature_sets.py" file
-    #   It's set up this way to easily add other feature sets later rather than
-    #   hard-coding feature-set-1, feature-set-2, and feature-set-3
-    #
-    # Furthermore, each of the feature sets in this dictionary will be a list
-    #   of csv files corresponding to each country and city
-    data = {}
+    # Loop through the cities in each country
+    cities = os.listdir("{}/{}".format(FEATURE_SET_DIRECTORY, country_name))
+    cities.sort()
+    return cities
 
-    # Loop through the feature-sets directory
-    for country_name in os.listdir(FEATURE_SET_DIRECTORY):
-        print("Loading {}\r".format(country_name))
-        # Loop through the cities in each country
-        for city_name in os.listdir("{}/{}".format(FEATURE_SET_DIRECTORY, country_name)):
+def get_data_frame(country, city, feature_set_number):
 
-            # Loop through each feature set
-            for feature_set in os.listdir("{}/{}/{}".format(FEATURE_SET_DIRECTORY, country_name, city_name)):
+    # Check if the city's directory exists
+    if not os.path.isdir("{}/{}/{}".format(FEATURE_SET_DIRECTORY, country, city)):
+        print("Couldn't find any feature sets for the city " + city)
+        print("Did you run 'python3 make-feature-sets.py'?")
+        exit(1)
 
-                # Trim off the ".csv"
-                if feature_set.endswith(".csv"):
-                    friendly_feature_set = feature_set.split(".")[0]
+    # See if the file we're looking for is in this directory
+    if "feature-set-{}.csv".format(feature_set_number) in os.listdir("{}/{}/{}".format(FEATURE_SET_DIRECTORY, country, city)):
 
-                if friendly_feature_set not in data:
-                    data[friendly_feature_set] = []
+        # Load the pandas dataframe for this feature set
+        data_frame = pandas.read_csv("{}/{}/{}/feature-set-{}.csv".format(FEATURE_SET_DIRECTORY, country, city, feature_set_number))
+        return data_frame
+    else:
+        print("Couldn't find feature set {} for country {} and city {}".format(feature_set_number, country, city))
 
-                # Load the pandas dataframe for this feature set
-                data_frame = pandas.read_csv("{}/{}/{}/{}".format(FEATURE_SET_DIRECTORY, country_name, city_name, feature_set))
-                
-                data[friendly_feature_set].append(data_frame)
-
-    return data
-
+    
 def prepare_data_split(raw_data, feature_type):
     #feature_type in 1, 2 or 3
     feature_key_str = Feature_Set_Map[feature_type]
@@ -149,8 +149,8 @@ def prepare_data_split(raw_data, feature_type):
     Y,X = feature_set_data[:,0], feature_set_data[:,1:]
     x_train, x_test, y_train, y_test = train_test_split(X,Y,test_size=.08)
     return x_train, x_test, y_train, y_test
-raw_data = get_data_feature_sets()
+# raw_data = get_data_feature_sets()
 
 #retrieve splitted data for feature set 1:
-x_train, x_test, y_train, y_test = prepare_data_split(raw_data, 1)
-print("Loading feature sets complete: \n-#training examples: {}\n-#test examples: {}".format(x_train.shape[0], x_test.shape[0]))
+# x_train, x_test, y_train, y_test = prepare_data_split(raw_data, 1)
+# print("Loading feature sets complete: \n-#training examples: {}\n-#test examples: {}".format(x_train.shape[0], x_test.shape[0]))
