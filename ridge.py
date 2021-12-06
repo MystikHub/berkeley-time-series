8d0dd0d1edd13cfcfefd6aa24eee098a59440c9f
MystikHub <shutanu@pm.me>
2021-12-06 23:00:06 +0000

    Added license terms to ridge.py

--- ridge.py
+++ ridge.py
@@ -1,3 +1,19 @@
+# Berkeley Earth Time Series Prediction
+# Copyright (C) 2021 MystikHub, jkanav12, ssq07
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from sklearn.linear_model import Ridge
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import KFold
